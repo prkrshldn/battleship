078107318ee8422d678275b5626360355b5b8f73
Th3Skeleton <prkrshldn@gmail.com>
2026-06-07 13:45:17 -0400

    main

--- src/util/shipGetInfo.js
+++
@@ -1,6 +0,0 @@
-// this is to track ship info based on select value
-
-export function getShipInfo(shipSelect, ships) {
-  const selectedKey = shipSelect.value;
-  return ships.find((ship) => ship.key === selectedKey) || null;
-}
